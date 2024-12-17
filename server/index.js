479e9d274d0e232c6a47721767b6e85930ed2e51
Kien Nguyen <39519090+kin203@users.noreply.github.com>
2024-12-17 15:57:42 +0700

    Update index.js
    fix token not found

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const {app,server}= require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL || '*',
-    credentials : true
-}))
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true, 
+}));
 app.use(express.json())
 app.use(cookieParse())
 
