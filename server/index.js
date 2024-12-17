b389cd995693bf7cb236f44adb3c32ffea93b785
Kien Nguyen <39519090+kin203@users.noreply.github.com>
2024-12-17 14:00:13 +0700

    Update index.js
    fix be fe test

--- server/index.js
+++ server/index.js
@@ -8,8 +8,7 @@ const {app,server}= require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    // origin : process.env.FRONTEND_URL,
-    origin : '*',
+    // origin : process.env.FRONTEND_URL || '*',
     credentials : true
 }))
 app.use(express.json())
