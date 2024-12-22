5e11c05b6184d928b115bf12238c96c8bbcc9b63
Kien Nguyen <39519090+kin203@users.noreply.github.com>
2024-12-23 02:28:19 +0700

    Update getConversation.js
    fix conflic

--- server/controller/getConversation.js
+++ server/controller/getConversation.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
 const getConversation = require("../helpers/getConversation");
 
@@ -31,37 +30,3 @@ async function getConversations(request, response) {
 }
 
 module.exports = getConversations;
-=======
-const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
-const getConversation = require("../helpers/getConversation");
-
-async function getConversations(request, response) {
-  try {
-    const token = request.cookies.token || "";
-
-    // Lấy thông tin người dùng từ token
-    const user = await getUserDetailsFromToken(token);
-    if (!user) {
-      return response.status(401).json({
-        message: "Unauthorized: Invalid token",
-        error: true,
-      });
-    }
-
-    // Lấy danh sách hội thoại của người dùng
-    const conversations = await getConversation(user._id);
-
-    return response.status(200).json({
-      message: "Conversations fetched successfully",
-      data: conversations,
-    });
-  } catch (error) {
-    return response.status(500).json({
-      message: error.message || "An error occurred",
-      error: true,
-    });
-  }
-}
-
-module.exports = getConversations;
->>>>>>> Stashed changes
